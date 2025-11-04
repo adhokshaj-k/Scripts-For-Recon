3afa08d6b6d2e5980f70959927460409fc4a91c1
Adhokshaj <144054981+adhokshaj-k@users.noreply.github.com>
2025-11-05 00:14:37 +0530

    Added some option in the script

--- SubD/subd.sh
+++ SubD/subd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+figlet "SubD"
 # Default values
 RATE_LIMIT=5
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
